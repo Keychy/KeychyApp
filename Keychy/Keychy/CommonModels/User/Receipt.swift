a0b7a578a40da62dbabe34b4421b5f299646ad42
giljihun <accc45@outlook.com>
2026-01-28 16:31:24 +0900

    chore: Receipt - 아이템 타입 표시, 구매일시 포맷 추가

--- Keychy/Keychy/CommonModels/User/Receipt.swift
+++ Keychy/Keychy/CommonModels/User/Receipt.swift
@@ -15,5 +15,23 @@ struct Receipt: Identifiable, Codable {
     var itemType: String
     var price: Int
     var purchasedAt: Date
-    
+
+    /// 아이템 타입 표시명
+    var itemTypeDisplayName: String {
+        switch itemType {
+        case "template": return "템플릿"
+        case "background": return "배경"
+        case "carabiner": return "카라비너"
+        case "particle": return "파티클"
+        case "sound": return "사운드"
+        default: return itemType
+        }
+    }
+
+    /// 구매일시 포맷 (2026-01-01 22:22:22)
+    var purchasedAtFormatted: String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        return formatter.string(from: purchasedAt)
+    }
 }
