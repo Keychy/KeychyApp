8aaa92f6dfd706ce432c007df97763d128632711
giljihun <accc45@outlook.com>
2026-02-10 21:24:18 +0900

    chore: 테스트용 네온사인키링 관련 파일 제거

---
+++ Keychy/Keychy/Core/Keyring/Scene/KeyringScale.swift
@@ -0,0 +1,15 @@
+//
+//  KeyringScale.swift
+//  Keychy
+//
+//  Created by 길지훈 on 2/10/26.
+//
+
+import Foundation
+
+/// 키링 스케일 중앙 관리
+/// - 화면별 기본 스케일
+/// - 템플릿별 커스텀 스케일
+enum KeyringScale {
+
+}
