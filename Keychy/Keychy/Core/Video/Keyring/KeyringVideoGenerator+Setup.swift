92c77208958e19ca571821fe13d66a8a2c3bef0d
giljihun <accc45@outlook.com>
2026-02-13 06:07:26 +0900

    style: 키링영상추출 백그라운드 clear -> .whiteBackGround

--- Keychy/Keychy/Core/Video/Keyring/KeyringVideoGenerator+Setup.swift
+++ Keychy/Keychy/Core/Video/Keyring/KeyringVideoGenerator+Setup.swift
@@ -36,10 +36,10 @@ extension KeyringVideoGenerator {
         scene.size = CGSize(width: sceneWidth, height: sceneHeight)
         scene.bind(to: viewModel)
 
-        // 배경 이미지 추가
+        // 배경 이미지 추가 (scene보다 여유 있게 배치)
         if let bgImage = backgroundImage {
             let backgroundNode = SKSpriteNode(texture: SKTexture(image: bgImage))
-            backgroundNode.size = CGSize(width: sceneWidth, height: sceneHeight)
+            backgroundNode.size = CGSize(width: sceneWidth * 1.5, height: sceneHeight * 1.5)
             backgroundNode.position = CGPoint(x: sceneWidth / 2, y: sceneHeight / 2)
             backgroundNode.zPosition = -1000
             scene.addChild(backgroundNode)
