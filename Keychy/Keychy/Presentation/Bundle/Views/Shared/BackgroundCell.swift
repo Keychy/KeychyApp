dfb8b422368de99587012211cc96b00eb9bc5d39
Jini <yjkwon424@gmail.com>
2026-02-13 08:10:06 +0900

    style: 배경 및 카라비너 유료 보유 라벨 디자인 변경

--- Keychy/Keychy/Presentation/Bundle/Views/Shared/BackgroundCell.swift
+++ Keychy/Keychy/Presentation/Bundle/Views/Shared/BackgroundCell.swift
@@ -44,23 +44,28 @@ struct BackgroundCell: View {
                             Spacer()
                             if background.isOwned {
                                 // 유료 + 보유
-                                Text("보유")
-                                    .typography(.suit12M)
-                                    .foregroundStyle(.white100)
-                                    .padding(.horizontal, 8)
-                                    .padding(.vertical, 1.5)
-                                    .background(.black60)
-                                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 20)
+                                        .fill(.black60)
+                                        .frame(width: 38, height: 20)
+                                    
+                                    Text("보유")
+                                        .typography(.suit12M)
+                                        .foregroundStyle(.white100)
+                                }
+                                
                             } else {
                                 // 유료 + 미보유: 가격 표시
-                                Text("\(background.background.price)")
-                                    .typography(.nanum13EB)
-                                    .foregroundStyle(.white100)
-                                    .padding(.horizontal, 6)
-                                    .padding(.vertical, 2.25)
-                                    .padding(.top, 2)
-                                    .background(.mainOpacity80)
-                                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 20)
+                                        .fill(.mainOpacity80)
+                                        .frame(width: 38,height: 20)
+                                    
+                                    Text("\(background.background.price)")
+                                        .typography(.nanum13EB)
+                                        .foregroundStyle(.white100)
+                                        .padding(.top, 1)
+                                }
                             }
                         }
                         Spacer()
