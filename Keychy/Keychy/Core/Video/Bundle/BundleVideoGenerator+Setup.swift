ff976ac8e615a16f18bd8a4132880358227bfa1e
Jini <yjkwon424@gmail.com>
2026-03-10 01:02:02 +0900

    feat: 뭉치 영상 생성시 하단에 워터마크 추가

--- Keychy/Keychy/Core/Video/Bundle/BundleVideoGenerator+Setup.swift
+++ Keychy/Keychy/Core/Video/Bundle/BundleVideoGenerator+Setup.swift
@@ -98,6 +98,19 @@ extension BundleVideoGenerator {
             scene.addChild(backgroundNode)
         }
 
+        if let watermarkImage = UIImage(named: "shareWaterMark") {
+            let watermarkNode = SKSpriteNode(texture: SKTexture(image: watermarkImage))
+            
+            let nodeWidth: CGFloat = 120
+            let nodeHeight: CGFloat = 30
+            watermarkNode.size = CGSize(width: nodeWidth, height: nodeHeight)
+            
+            let bottomMargin = (16 + nodeHeight / 2)
+            watermarkNode.position = CGPoint(x: sceneWidth / 2, y: bottomMargin)
+            watermarkNode.zPosition = 999
+            scene.addChild(watermarkNode)
+        }
+        
         scene.onSetupComplete = {
             setupComplete()
         }
