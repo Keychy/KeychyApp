d0092482d0b2640a5e8946381644ccd66cd92bd8
giljihun <accc45@outlook.com>
2026-05-27 16:33:55 +0900

    style(sticker): BIG 컬렉션뷰 하단 안전 마진 18 → 14pt
    너무 떨어져 보여서 4pt만 좁힘 (constant -50 → -46)

--- Keychy/MessageKeychy/Carousel/StickerCarouselVC.swift
+++ Keychy/MessageKeychy/Carousel/StickerCarouselVC.swift
@@ -173,8 +173,8 @@ class StickerCarouselVC: UIViewController {
             bigCollectionView.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: 8),
             bigCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             bigCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            // 힌트 라벨(view.bottom-14, 약 18pt 높이) 위로 충분한 여백 확보
-            bigCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50),
+            // 힌트 라벨(view.bottom-14, 약 18pt 높이) 위로 약 14pt 여백
+            bigCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -46),
         ])
     }
 
